3923c03e39ec7599ea2a1392b9894b9535cfcce5
if-water <502395085@qq.com>
2022-10-25 07:56:37 +0000

    for and if-else practice

--- practice/2.if-else/main.go
+++ practice/2.if-else/main.go
@@ -1,11 +1,20 @@
 package main
+
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Printf("test")
-	if 1==2 {
+	if 1 == 2 {
 		fmt.Printf("")
-	}else{
+	} else {
 		fmt.Printf("1!=2")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
 }
